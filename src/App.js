47c16ecb187a61bab5aac3319120dde4e041f025
rkshs <rakshas131@gmail.com>
2022-08-31 06:58:11 +0400

    auto start, css improve + simple animations, cards match

--- src/App.js
+++ src/App.js
@@ -1,32 +1,36 @@
-import { useState } from 'react';
+import {useEffect, useState} from 'react';
 import Card from './components/Card'
 import './App.css';
 
  const cardImages = [
-     {'className': 'card-image-1'},
-     {'className': 'card-image-2'},
-     {'className': 'card-image-3'},
-     {'className': 'card-image-4'},
-     {'className': 'card-image-5'},
-     {'className': 'card-image-6'},
-     {'className': 'card-image-7'},
-     {'className': 'card-image-8'},
-     {'className': 'card-image-9'},
-     {'className': 'card-image-10'},
-     {'className': 'card-image-11'},
-     {'className': 'card-image-12'},
-     {'className': 'card-image-13'},
-     {'className': 'card-image-14'},
-     {'className': 'card-image-15'},
-     {'className': 'card-image-16'},
-     {'className': 'card-image-17'},
-     {'className': 'card-image-18'},
+     {'className': 'card-image-1', matched: false},
+     {'className': 'card-image-2', matched: false},
+     {'className': 'card-image-3', matched: false},
+     {'className': 'card-image-4', matched: false},
+     {'className': 'card-image-5', matched: false},
+     {'className': 'card-image-6', matched: false},
+     {'className': 'card-image-7', matched: false},
+     {'className': 'card-image-8', matched: false},
+     {'className': 'card-image-9', matched: false},
+     {'className': 'card-image-10', matched: false},
+     {'className': 'card-image-11', matched: false},
+     {'className': 'card-image-12', matched: false},
+     {'className': 'card-image-13', matched: false},
+     {'className': 'card-image-14', matched: false},
+     {'className': 'card-image-15', matched: false},
+     {'className': 'card-image-16', matched: false},
+     {'className': 'card-image-17', matched: false},
+     {'className': 'card-image-18', matched: false},
  ]
 
 function App() {
 
     const [cards, setBunchCards] = useState([]);
     const [turn, setTurn] = useState(0);
+    const [chosenOne, setChosenOne] = useState(null);
+    const [chosenTwo, setChosenTwo] = useState(null);
+    const [disabled, setDisabled] = useState(false);
+
 
     const shuffle = () => {
     const newBunchCards = [...cardImages, ...cardImages]
@@ -34,22 +38,66 @@ function App() {
         .map((el) => {
         return {...el, id: Math.random()}
     })
-        console.log(newBunchCards)
 
+        setChosenOne(null);
+        setChosenTwo(null);
         setBunchCards(newBunchCards);
         setTurn(0)
     }
 
-    console.log(cards, turn)
+    const setChoose = (card) => {
+        chosenOne ? setChosenTwo(card) : setChosenOne(card);
+    }
+
+    useEffect(() => {
+        if (chosenOne && chosenTwo) {
+            setDisabled(true);
+
+            if (chosenOne.className === chosenTwo.className) {
+                setBunchCards(newBunch => {
+                    return newBunch.map(card => {
+                        if (card.className === chosenOne.className) {
+                            return {...card, matched: true}
+                        } else {
+                            return card
+                        }
+                    })
+                })
+                reseter();
+            } else {
+                setTimeout(() => reseter(), 1000);
+            }
+        }
+    }, [chosenOne, chosenTwo])
+
+    const reseter = () => {
+        setChosenOne(null);
+        setChosenTwo(null);
+        setTurn(t => t + 1);
+        setDisabled(false);
+    }
+
+    useEffect(() => {
+        shuffle();
+    }, [])
 
     return (
     <div className="App">
         <h1>Find a pair game</h1>
-        <button onClick={shuffle}>New game</button>
+        <div>
+            <button onClick={shuffle}>New game</button>
+            <span>Turns: {turn}</span>
+        </div>
         <div className='grid'>
             {
                 cards.map(card => (
-                    <Card key={card.id} card={card}/>
+                    <Card
+                        key={card.id}
+                        card={card}
+                        setChoose={setChoose}
+                        flip={card === chosenOne || card === chosenTwo || card.matched}
+                        disabled={disabled}
+                    />
                 ))
             }
         </div>
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,16 @@
 import './Card.css';
 
-export default function Card({ card }) {
+export default function Card({ card, setChoose, flip, disabled }) {
+
+    const clickHandler = () => {
+        if (!disabled) {
+            setChoose(card)
+        }
+    }
+
     return (
-        <div key={card.id} className='card'>
-            <div className='card-cover' />
+        <div key={card.id} className={`card ${flip ? 'flip' : ''}`}>
+            <div className='card-cover' onClick={clickHandler} />
             <div className={`card-image ${card.className}`} />
         </div>
     )
